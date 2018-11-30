b2c704a43b4331828a43e20b149700b3887452b3
xiaoxiae <tomas.slama.131@gmail.com>
2018-11-30 16:07:11 +0100

    fix clean_facebook_data.py
    The script was accidentally deleting a lot more than it should have.
    
    It is now fixed and also deletes empty directories.

--- Úloha 3/dat/clean_facebook_data.py
+++ Úloha 3/dat/clean_facebook_data.py
@@ -1,13 +1,15 @@
-"""A script for removing all non-.json (and non-.py) files in all
-subdirectories of the current directory. Used to clean up unnecessary binary
-files from the Facebook data."""
-import json
+"""A script for removing all unnecessary Facebook data in all subdirectories
+of the 'messages' directory."""
 import os
 
-# Find all non-json files (and non-py files)
-for path, dirs, files in os.walk('.'):
+for path, dirs, files in os.walk('messages/'):
+    # Find all unnecessary Facebook files
     for file in files:
 
-        # Remove them
-        if not file.endswith('.json') and file.endswith('.py'):
+        if not file.endswith(".json"):
             os.remove(os.path.join(path, file))
+
+    # Remove directories that are empty (after file deletion)
+    for dir in dirs:
+        if not os.listdir(os.path.join(path, dir)):
+            os.rmdir(os.path.join(path, dir))
