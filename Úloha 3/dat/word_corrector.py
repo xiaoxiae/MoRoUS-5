758b689a4c1291520d0a41006e1fdfaad17c3089
xiaoxiae <tomas.slama.131@gmail.com>
2018-11-30 12:17:55 +0100

    comment and refactor word_corrector.py
    Commented the functions and refactored the creation of the widgets.

--- Úloha 3/dat/word_corrector.py
+++ Úloha 3/dat/word_corrector.py
@@ -11,61 +11,79 @@ class WordCorrector(QWidget):
         """Inicializace prvků aplikace."""
         super().__init__(*args, **kwargs)
 
-        # VYTVOŘENÍ OVLÁDACÍCH PRVKŮ
-        self.textboxLabel = QLabel(self)
-        self.textboxLabel.setAlignment(Qt.AlignCenter)
+        # LABEL CREATION
+        self.headingLabel = QLabel(self,
+            alignment = Qt.AlignCenter,
+            text = "<h3>Word Corrector</h3>",
+            sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred))
 
-        self.percentageLabel = QLabel(self)
-        self.percentageLabel.setAlignment(Qt.AlignRight)
+        # Perecentage of the text corrected
+        self.percentageLabel = QLabel(self,
+            alignment = Qt.AlignRight)
 
-        self.wordLabel = QLabel(self)
-        self.wordLabel.setAlignment(Qt.AlignCenter)
-        self.wordLabel.setWordWrap(True)
+        # Words to be corrected
+        self.wordLabel = QLabel(self,
+            text = "Press <b>Start</b> to begin.",
+            alignment = Qt.AlignCenter,
+            wordWrap = True,
+            sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred))
 
-        self.startButton = QPushButton(self, text = "Start")
-        self.startButton.clicked.connect(self.start)
+        # BUTTON CREATION
+        self.startButton = QPushButton(self,
+            text = "Start",
+            clicked = self.start)
 
-        self.includeButton = QPushButton(self, text = "Include", styleSheet = 'QPushButton {color: green;}', enabled=False)
-        self.includeButton.clicked.connect(self.include)
-        self.excludeButton = QPushButton(self, text = "Exclude", styleSheet = 'QPushButton {color: red;}', enabled=False)
-        self.excludeButton.clicked.connect(self.exclude)
-        self.correctButton = QPushButton(self, text = "Adjust", styleSheet = 'QPushButton {color: blue;}', enabled=False)
-        self.correctButton.clicked.connect(self.adjust)
+        self.includeButton = QPushButton(self,
+            text = "Include",
+            styleSheet = 'QPushButton {color: green;}',
+            enabled=False,
+            clicked = self.include)
 
-        self.textbox = QLineEdit(self)
-        self.textbox.returnPressed.connect(self.correctButton.click)
+        self.excludeButton = QPushButton(self,
+            text = "Exclude",
+            styleSheet = 'QPushButton {color: red;}',
+            enabled=False,
+            clicked = self.exclude)
 
-        # ROZLOŽENÍ OVLÁDACÍCH PRVKŮ
-        self.layout = QVBoxLayout(self)
+        self.correctButton = QPushButton(self,
+            text = "Adjust",
+            styleSheet = 'QPushButton {color: blue;}',
+            enabled=False,
+            clicked = self.adjust)
 
+        # Word textbox
+        self.textbox = QLineEdit(self,
+            returnPressed = self.correctButton.click)
+
+        # LAYOUT SETUP
+        self.mainVLayout = QVBoxLayout(self)
+
+        # Heading and percentage
         self.labelHLayout = QHBoxLayout(self)
-        self.labelHLayout.addWidget(self.textboxLabel)
+        self.labelHLayout.addWidget(self.headingLabel)
         self.labelHLayout.addWidget(self.percentageLabel)
-        self.textboxLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-        self.textboxLabel.setText("<h3>Word Corrector</h3>")
-        self.layout.addLayout(self.labelHLayout)
 
-        # Layout for the textbox and start button
-        self.mainHLayout = QHBoxLayout(self)
-        self.mainHLayout.addWidget(self.textbox)
-        self.mainHLayout.addWidget(self.startButton)
-        self.layout.addLayout(self.mainHLayout)
+        # Textbox and startbutton
+        self.centerHLayout = QHBoxLayout(self)
+        self.centerHLayout.addWidget(self.textbox)
+        self.centerHLayout.addWidget(self.startButton)
 
-        # Layout for the words to be corrected
-        self.layout.addWidget(self.wordLabel)
-        self.wordLabel.setText("Press <b>Start</b> to begin.")
-        self.wordLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-
-        # Layout for the buttons
+        # The include, exclude and correct buttons
         self.buttonHLayout = QHBoxLayout(self)
         self.buttonHLayout.addWidget(self.includeButton)
         self.buttonHLayout.addWidget(self.excludeButton)
         self.buttonHLayout.addWidget(self.correctButton)
-        self.layout.addLayout(self.buttonHLayout)
 
-        # Zobrazení
-        self.setLayout(self.layout)
+        # Add all the h-layouts to the main v-layout
+        self.mainVLayout.addLayout(self.labelHLayout)
+        self.mainVLayout.addLayout(self.centerHLayout)
+        self.mainVLayout.addWidget(self.wordLabel)
+        self.mainVLayout.addLayout(self.buttonHLayout)
+
+        # Show the main layout
+        self.setLayout(self.mainVLayout)
         self.show()
+
 
     def start(self):
         # Disable/enable the appropriate buttons
@@ -74,11 +92,10 @@ class WordCorrector(QWidget):
         self.includeButton.setEnabled(True)
         self.excludeButton.setEnabled(True)
 
-        # Load the Czech dictionary
+        # Load the dictionaries
         cz_dict_file = codecs.open("dictionaries/czech.dic", "r", encoding="utf-8")
         self.cz_dict = cz_dict_file.read().splitlines()
 
-        # Load the English dictionary
         en_dict_file = open("dictionaries/english.dic", "r")
         self.en_dict = en_dict_file.read().splitlines()
 
@@ -90,14 +107,18 @@ class WordCorrector(QWidget):
         # A dictionary to store corrections
         self.corrections = {}
 
-        # Start going through the messages
+        # Variables to track the current word
         self.sentenceNumber = 0
         self.wordNumber = 0
 
+        # Start going through the messages
         self.setPercentageLabel()
         self.goThroughMessages()
 
+
     def goThroughMessages(self):
+        """Goes through the words in the sentence list and stops when it
+        reaches one that needs to be corrected."""
         # Go through all of the sentences
         while self.sentenceNumber < len(self.sentences):
             sentence = self.sentences[self.sentenceNumber]
@@ -129,6 +150,9 @@ class WordCorrector(QWidget):
 
                     # Else set the word label accordingly and return
                     self.setWordLabel(word, sentence)
+
+                    # Set the percentage label before returning
+                    self.setPercentageLabel()
                     return
 
                 self.wordNumber += 1
@@ -136,9 +160,11 @@ class WordCorrector(QWidget):
             self.wordNumber = 0
             self.sentenceNumber += 1
 
-            self.setPercentageLabel()
 
     def setWordLabel(self, word, sentence):
+        """Set the word label to the current sentence, with the word to be
+        corrected highlighted in bold."""
+
         prettySentence = ""
 
         # TODO: Fix the shortening of the sentence
@@ -163,33 +189,49 @@ class WordCorrector(QWidget):
 
         self.wordLabel.setText(prettySentence.strip())
 
-    def setPercentageLabel(self):
-        # The percentage of the words that we've been through
-        percentage = "{0:.2f}%".format(self.sentenceNumber / len(self.sentences) * 100)
 
-        self.percentageLabel.setText(percentage)
+    def setPercentageLabel(self):
+        """Sets the percentageLabel to the percentage of the words that
+        we've been through."""
+
+        percentage = self.sentenceNumber / len(self.sentences) * 100
+        formated_percentage = "{0:.2f}%".format(percentage)
+
+        self.percentageLabel.setText(formated_percentage)
+
 
     def include(self):
+        """Add the current word to the czech dictionary and run
+         goThroughMessages."""
+
         word = self.sentences[self.sentenceNumber][self.wordNumber]
 
-        # Find the position of where it should be and add it to the dictionary
+        # Find the position of where it should be and add it there
         cz_i = bisect_left(self.cz_dict, word)
         self.cz_dict.insert(cz_i, word)
 
         self.goThroughMessages()
 
+
     def exclude(self):
-        # Set the word (and it's correction) to - (like it's not there)
+        """Set the correction of the word to "-" and run goThroughMessages."""
+
         self.corrections[self.sentences[self.sentenceNumber][self.wordNumber]] = "-"
 
         self.goThroughMessages()
 
+
     def adjust(self):
+        """Set the correction of the word to whatever is in the textbox and
+        run goThroughMessages."""
+
+        self.textbox.clear()
+
         # Get the word and it's correction
         wordCorrection = self.textbox.text()
         word = self.sentences[self.sentenceNumber][self.wordNumber]
 
-        # If the person pressed enter on a correct word, simply include it
+        # If called on an empty textbox, include the word in the dictionary
         if len(wordCorrection) == 0:
             self.include()
             return
@@ -197,11 +239,9 @@ class WordCorrector(QWidget):
         # Add the correction for the future
         self.corrections[word] = wordCorrection
 
-        self.textbox.clear()
-
         self.goThroughMessages()
 
-# Spuštění celého programu
+# Run the app
 app = QApplication(sys.argv)
-ordCorrector = WordCorrector()
+wordCorrector = WordCorrector()
 app.exec()
