c8547df168de212a8aff6f6d283f9bd6f2f691b8
xiaoxiae <tomas.slama.131@gmail.com>
2018-12-01 11:56:24 +0100

    add autocomplete.py
    Add a program that takes the corrected messages and performs autocomplete suggetions on sentences typed in it's textbox.

---
+++ Úloha 3/autocomplete.py
@@ -0,0 +1,171 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit
+from PyQt5.QtCore import Qt
+import re
+import codecs
+
+import sys
+
+class Autocomplete(QWidget):
+    def __init__(self, *args, **kwargs):
+        """Inicializace prvků aplikace."""
+        super().__init__(*args, **kwargs)
+
+        # VYTVOŘENÍ OVLÁDACÍCH PRVKŮ
+        self.headingLabel = QLabel(self,
+            text = "<h3>Autocomplete</h3>",
+            alignment = Qt.AlignCenter)
+
+        self.informationLabel = QLabel(self,
+            alignment = Qt.AlignCenter)
+
+        self.textbox = QLineEdit(self,
+            textChanged = self.textboxChanged)
+
+        self.suggestionLabel = QLabel(self,
+            alignment = Qt.AlignCenter)
+
+        # ROZLOŽENÍ OVLÁDACÍCH PRVKŮ
+        self.layout = QVBoxLayout(self)
+
+        self.layout.addWidget(self.headingLabel)
+        self.layout.addWidget(self.textbox)
+        self.layout.addWidget(self.informationLabel)
+
+        # Generates the trie from the messages.txt file
+        self.generateTrie()
+
+        # Create an "allWords" set that includes all known words from the trie
+        list = []
+        self.getTrieWords(self.trie, "", list)
+        self.allWords = set(word[0] for word in list)
+
+        # Show the layout
+        self.setLayout(self.layout)
+        self.show()
+
+
+    def trieWordValue(self, word):
+        """Returns the frequency of the word if the word is in self.trie and
+        None if it is not."""
+        # Walk the trie until the last word character
+        trieBranch = self.trie
+        for letter in word[:-1]:
+            if letter in trieBranch:
+                trieBranch = trieBranch[letter][0]
+            else:
+                return None
+
+        # If the frequency of the word in the trie isn't 0
+        if word[-1] in trieBranch and trieBranch[word[-1]][1] != 0:
+            return trieBranch[word[-1]][1]
+        else:
+            return None
+
+
+    def generateTrie(self):
+        """Generates a trie from the dat/messages.txt file"""
+        self.trie = {}
+
+        # Read lines of the file one by one
+        with codecs.open("dat/corrected_messages.txt", encoding="UTF-8") as f:
+            for line in f:
+
+                for word in line.strip().split(" "):
+                    trieBranch = self.trie
+
+                    # Go through all the characters
+                    for i in range(len(word)):
+                        character = word[i]
+
+                        # If this character is not in a trie, add it
+                        if character not in trieBranch:
+                            trieBranch[character] = [{}, 0]
+
+                        # If we are at the end of the word, increment frequency
+                        if i == len(word) - 1:
+                            trieBranch[character][1] += 1
+
+                        # Go further into the branch
+                        trieBranch = trieBranch[character][0]
+
+
+    def textboxChanged(self, text):
+        """Funkce volána s každou změnou textboxu."""
+        # Match the last word
+        regex = "\s*([a-zěščřžýáíéóňďťůú]+)$"
+        wordSeach = re.search(regex, text.strip())
+
+        # If there is no word, return
+        if wordSeach == None:
+            return
+
+        word = wordSeach.group(0).strip()
+
+        # Traverse the trie
+        trieBranch = self.trie
+        for letter in word:
+            # Traverse if we can and write possible corrections if we can't
+            if letter in trieBranch:
+                trieBranch = trieBranch[letter][0]
+            else:
+                correction = self.getWordCorrections(word)
+                predictions = " ".join([word[0] for word in correction[0:5]])
+                self.informationLabel.setText(predictions)
+                return
+
+        # Get all of the possible words from the trie
+        predictions = self.getPredictionsFromTrie(trieBranch, word)
+
+        # Set the information label as the first 5 recommendations
+        predictionString = " ".join([word[0] for word in predictions[0:5]])
+        self.informationLabel.setText(predictionString)
+
+
+    def getWordCorrections(self, word):
+        """Returns possible corrections of a word."""
+        # Gets the words that are one edit away and are in the allWords set
+        words = [w for w in self.wordEdits(word) if w in self.allWords]
+
+        # Sort them by their values in trie
+        sortedWords = sorted([(w, self.trieWordValue(w)) for w in words],
+            key=lambda x:x[1])
+
+        return sortedWords
+
+
+    def wordEdits(self, word):
+        """Returns edits that are one away from the word as a set."""
+
+        letters    = 'abcdefghijklmnopqrstuvwxyzěščřžýáíéóňďťůú'
+        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+
+        deletes    = [L + R[1:]               for L, R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+        replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+        inserts    = [L + c + R               for L, R in splits for c in letters]
+
+        return set(deletes + transposes + replaces + inserts)
+
+
+    def getPredictionsFromTrie(self, trie, word):
+        """Return words from a trie in a sorted order."""
+        list = []
+        self.getTrieWords(trie, word, list)
+        return sorted(list, key=lambda x:x[1], reverse=True)
+
+
+    def getTrieWords(self, trie, word, list):
+        """Adds the words and their frequencies from the trie to the list."""
+        for letter, letterData in trie.items():
+            # If the frequency isn't 0, add it to the list
+            if letterData[1] != 0:
+                list.append((word + letter, letterData[1]))
+
+            # Recursively do the same for the rest of the trie branch
+            self.getTrieWords(trie[letter][0], word + letter, list)
+
+
+# Spuštění celého programu
+app = QApplication(sys.argv)
+crossing = Autocomplete()
+app.exec()
